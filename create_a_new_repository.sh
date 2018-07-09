fe3d5e276faf9bc1ea125dcded95c03549fb0bf0
王小龙 <lucky@Luckys-MBP.local>
2018-07-09 12:05:30 +0800

    first commit 1

---
+++ create_a_new_repository.sh
@@ -0,0 +1,6 @@
+echo "# YiTaoDashBoard" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/AndroidIosWebEmbeddedFirmwareDver/YiTaoDashBoard.git
+git push -u origin master
---
+++ push_an_existing_repository.sh
@@ -0,0 +1,2 @@
+git remote add origin https://github.com/AndroidIosWebEmbeddedFirmwareDver/YiTaoDashBoard.git
+git push -u origin master
